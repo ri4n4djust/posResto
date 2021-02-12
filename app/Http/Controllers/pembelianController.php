b12a479af3b8c01737fd1bb46f743b195f0b04d3
ri4n4djust <rian.adjust@gmail.com>
2021-02-12 08:27:26 +0800

    New

---
+++ app/Http/Controllers/pembelianController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class pembelianController extends Controller
+{
+    //
+}
---
+++ database/migrations/2021_02_12_082257_pembelian.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Pembelian extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('tblPembelian', function (Blueprint $table) {
+            $table->id();
+            $table->string('idSupplier');
+            $table->string('noNotaPembelian');
+            $table->string('tglNotaPembelian');
+            $table->string('totalNotaPembelian');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('tblPembelian');
+    }
+}
