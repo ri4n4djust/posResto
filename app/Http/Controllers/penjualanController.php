e9679ee5ab76fc75e83759cb87b924f77f371ea3
ri4n4djust <rian.adjust@gmail.com>
2022-12-29 19:17:40 +0800

    Update penjualanController.php

--- app/Http/Controllers/penjualanController.php
+++ app/Http/Controllers/penjualanController.php
@@ -23,6 +23,7 @@ class penjualanController extends Controller
                 'tblPembayaran.pajakPembayaran', 'tblPembayaran.diskonPembayaran',
                 'tblPembayaran.typePembayaran', 'tblPembayaran.chargePembayaran', 'tblPembayaran.noKartuPembayaran')
                 ->orderBy('tblPenjualan.id', 'DESC')
+                ->take(100)
                 ->get();
         return response([
             'success' => true,
